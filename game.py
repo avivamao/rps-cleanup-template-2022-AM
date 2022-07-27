66b02e8abc7780993218d70ad313d6f72388ffad
avivamao <108594351+avivamao@users.noreply.github.com>
2022-07-27 15:31:52 -0400

    Use the function we tested/fix the bug

--- game.py
+++ game.py
@@ -61,23 +61,6 @@ if __name__ == "__main__":
     # DETERMINATION OF WINNER
     #
 
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
+    # Use the function from above "winner"
 
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
-
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    print(winner(u,c))
